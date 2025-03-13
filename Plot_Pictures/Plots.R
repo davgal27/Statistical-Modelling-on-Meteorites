3e2d2a132092a69cf8e4513b50cb400534e83305
Dave Galea <119041022+davgal27@users.noreply.github.com>
2025-03-13 18:52:35 +0100

    Update Plots.R

--- Plot_Pictures/Plots.R
+++ Plot_Pictures/Plots.R
@@ -17,26 +17,17 @@ meteorite_map <- ggplot() +
   coord_fixed(ratio = 1.1)  
 print(meteorite_map)
 
-################ TIME SERIES PLOT 
-ggplot(meteorite_counts, aes(x = year, y = count)) +
-  geom_line(color = "black", size = 1) +  
-  geom_point(color = "red", size = 1.5) +  
-  labs(title = "Meteorite Discoveries Over Time",
-       x = "Year",
-       y = "Number of Discoveries") +
-  theme_minimal()  
-
 ############### HEAT MAP PLOT 
 world_map <- map_data("world")
 ggplot() +
   geom_map(data = world_map, map = world_map, aes(x = long, y = lat, map_id = region), 
            color = "black", fill = "lightgray", size = 0.2) +
   
- 
+  
   geom_density2d(data = meteorite_dataset, aes(x = reclong, y = reclat), color = "red", size = 0.5) +
   
   stat_density2d(data = meteorite_dataset, aes(x = reclong, y = reclat, fill = ..density..), 
-                 geom = "raster", contour = FALSE, alpha = 0.6) +  
+                 geom = "raster", contour = FALSE, alpha = 0.3) +  
   scale_fill_gradient(low = "white", high = "red") +  
   
   
@@ -45,6 +36,15 @@ ggplot() +
        y = "Latitude") +
   theme_minimal() +
   coord_fixed(ratio = 1.1)  
+
+################ TIME SERIES PLOT 
+ggplot(meteorite_counts, aes(x = year, y = count)) +
+  geom_line(color = "black", size = 1) +  
+  geom_point(color = "red", size = 1.5) +  
+  labs(title = "Meteorite Discoveries Over Time",
+       x = "Year",
+       y = "Number of Discoveries") +
+  theme_minimal()  
 
 ################ BOX PLOT MASS 
 
